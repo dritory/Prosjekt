19bc23c70b86e116c411fb495a05b22cd103942e
dritory <lolskap@live.no>
2021-05-19 23:49:35 +0200

    added missing files

---
+++ Code/run_ffmpeg.py
@@ -0,0 +1,14 @@
+
+import subprocess
+
+package_size_bytes = 16
+sample_rate = "16k" #hertz
+bit_rate = "8k"
+
+compression_level = "5"
+page_duration = "500000"
+
+file_name = "RadioGaGa.mp3"
+port = 26363
+
+subprocess.run(["ffmpeg", "-re", "-y", "-i",file_name,"-vn", "-ac", "1","-b:a", bit_rate,"-ar",sample_rate,"-compression_level",compression_level, "-application", "audio","-page_duration", page_duration,"-c:a","libopus","-f","ogg","udp://127.0.0.1:"+ str(port) + "?pkt_size="+str(package_size_bytes),])
---
+++ Code/udp_relay.py
@@ -0,0 +1,20 @@
+import socket
+import time
+
+IP = "127.0.0.1"
+source_port = 36363
+sink_port = 46363
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+sock.bind((IP, source_port))
+
+sock2 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+
+last = time.time()
+while True:
+    elapsed =  time.time() - last
+    last = time.time()
+    data, addr = sock.recvfrom(16)
+    #print(len(data),data.decode("ansi", errors = 'strict'))
+    sock2.sendto(data, (IP, sink_port))
